a7945b1e84d61d4f5c52531343ba9c6ce7283e89
Vedang Danej <vedangdanej34@gmail.com>
2022-11-05 13:42:44 +0530

    CORS allowed

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/animals', animalRoutes);
